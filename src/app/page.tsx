e62c39e77253f7a79951583bb92afe8b1136be0e
kartik-branch <kartik.shandilya@branch.io>
2024-09-25 10:36:50 +0530

    setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,10 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          src="/nextjs-github-pages/vercel.svg"
+          alt="Vercel Logo"
+          width={100}
+          height={24}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
